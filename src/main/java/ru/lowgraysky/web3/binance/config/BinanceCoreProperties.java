23b86aaad68816f3ee486b44050c3243e9b627c7
lowgraysky <nick.alexeevkz@gmail.com>
2023-08-30 20:31:54 +0600

    Correct configuration

--- src/main/java/ru/lowgraysky/web3/binance/config/BinanceCoreProperties.java
+++ src/main/java/ru/lowgraysky/web3/binance/config/BinanceCoreProperties.java
@@ -1,11 +1,13 @@
 package ru.lowgraysky.web3.binance.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @ConfigurationProperties(prefix = "binance.core")
 public class BinanceCoreProperties {
   private String baseUrl;
--- src/main/java/ru/lowgraysky/web3/configuration/ApplicationConfiguration.java
+++ src/main/java/ru/lowgraysky/web3/configuration/ApplicationConfiguration.java
@@ -1,20 +1,17 @@
 package ru.lowgraysky.web3.configuration;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.lowgraysky.web3.binance.config.BinanceCoreProperties;
 import ru.lowgraysky.web3.binance.config.BinancePathProperties;
 
 @Configuration
+@EnableConfigurationProperties({BinanceCoreProperties.class, BinancePathProperties.class})
 public class ApplicationConfiguration {
 
   @Bean
-  public BinancePathProperties binancePathProperties() {
-    return new BinancePathProperties();
-  }
-
-  @Bean
-  public BinanceCoreProperties binanceCoreProperties() {
-    return new BinanceCoreProperties();
+  public RouterConfiguration routerConfiguration() {
+    return new RouterConfiguration();
   }
 }
